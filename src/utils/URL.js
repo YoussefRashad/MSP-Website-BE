301bb616999b3a614cb40676b2c0c9bb57e0c83b
Youssef Rashad <youssefrashad119@gmail.com>
2021-01-12 15:09:00 +0200

    connects to production env

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,9 +1,9 @@
 
-const BE_URL = 'http://localhost:3000';
-const DB_URL = 'mongodb://127.0.0.1:27017/MSP-Tech-Club'
+// const BE_URL = 'http://localhost:3000';
+// const DB_URL = 'mongodb://127.0.0.1:27017/MSP-Tech-Club'
 
-// const BE_URL = 'https://msp-tech-club-helwan.netlify.app'
-// const DB_URL = 'mongodb+srv://YoussefRashad:0000112@msp-tech-club.cfjku.mongodb.net/MSP-Tech-Club?retryWrites=true&w=majority'
+const BE_URL = 'https://msp-tech-club-helwan.netlify.app'
+const DB_URL = 'mongodb+srv://YoussefRashad:0000112@msp-tech-club.cfjku.mongodb.net/MSP-Tech-Club?retryWrites=true&w=majority'
 
 module.exports = {
     BE_URL,
