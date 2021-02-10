d3ada41be47901e822147e3bc74d917fdcdcbb54
Youssef Rashad <40126657+YoussefRashad@users.noreply.github.com>
2021-02-10 23:49:31 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(cors({
     credentials: true
 }));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', URL +', https://msp-tech-club-egypt2.netlify.app');
+    res.header('Access-Control-Allow-Origin', URL);
     res.header('Access-Control-Allow-Headers', true);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
